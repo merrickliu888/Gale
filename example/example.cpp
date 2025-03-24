81b3579272d629101b09af043167a5f4da12a352
Merrick Liu <merrickliu168@gmail.com>
2025-03-24 00:05:46 -0400

    example comments

---
+++ example/example.cpp
@@ -0,0 +1,14 @@
+// include necessary headers
+
+int main() {
+    // initialize http server
+    // HttpServer server = new HttpServer();
+
+    // Register routes
+    // server.registerRoute(...);
+    // server.registerRoute(...);
+
+    // server.start();
+
+    return 0;
+}
